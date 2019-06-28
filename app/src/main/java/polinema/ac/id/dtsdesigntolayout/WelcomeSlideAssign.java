6bcbd985098a15e70040b02d198f24b92e3ddbc1
Putra Prima Arhandi <putraprima@gmail.com>
2019-06-28 20:12:35 +0700

    start solution

---
+++ app/src/main/java/polinema/ac/id/dtsdesigntolayout/WelcomeSlideAssign.java
@@ -0,0 +1,13 @@
+package polinema.ac.id.dtsdesigntolayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomeSlideAssign extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_slide_assign);
+    }
+}
