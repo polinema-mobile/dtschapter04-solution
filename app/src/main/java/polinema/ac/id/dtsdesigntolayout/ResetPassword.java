bfc2fc6b44eae3949487fb34431ae6156e56b0a7
Putra Prima Arhandi <putraprima@gmail.com>
2019-06-28 22:28:11 +0700

    kurang sak layout

---
+++ app/src/main/java/polinema/ac/id/dtsdesigntolayout/ResetPassword.java
@@ -0,0 +1,13 @@
+package polinema.ac.id.dtsdesigntolayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ResetPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+    }
+}
