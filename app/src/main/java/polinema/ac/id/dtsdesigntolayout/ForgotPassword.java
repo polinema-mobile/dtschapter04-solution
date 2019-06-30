08c8bff68245f1136638345f5c8dd9a7d81791b6
afif hendrawan <afif.hendrawan@gmail.com>
2019-06-30 13:32:23 +0700

    solution for validation

--- app/src/main/java/polinema/ac/id/dtsdesigntolayout/ForgotPassword.java
+++ app/src/main/java/polinema/ac/id/dtsdesigntolayout/ForgotPassword.java
@@ -3,18 +3,42 @@ package polinema.ac.id.dtsdesigntolayout;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ForgotPassword extends AppCompatActivity {
+
+    EditText editTextEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+
+        editTextEmail = findViewById(R.id.edt_txt_email);
     }
 
     public void postSendRequest(View view) {
-        Intent i = new Intent(ForgotPassword.this, ResetPassword.class);
-        startActivity(i);
+        if(TextUtils.isEmpty(editTextEmail.getText().toString().trim())) {
+            Toast.makeText(view.getContext(), "Email tidak boleh kosong!", Toast.LENGTH_LONG).show();
+        } else if (!isValidEmail(editTextEmail.getText().toString().trim())) {
+            Toast.makeText(view.getContext(), "Email tidak valid!", Toast.LENGTH_LONG).show();
+        } else {
+            Intent i = new Intent(ForgotPassword.this, ResetPassword.class);
+            startActivity(i);
+        }
+    }
+
+    /**
+     * Email validation
+     *
+     * @param email : email input from editText
+     * @return
+     */
+    public static boolean isValidEmail(CharSequence email) {
+        return (Patterns.EMAIL_ADDRESS.matcher(email).matches());
     }
 }
--- app/src/main/java/polinema/ac/id/dtsdesigntolayout/ResetPassword.java
+++ app/src/main/java/polinema/ac/id/dtsdesigntolayout/ResetPassword.java
@@ -3,18 +3,41 @@ package polinema.ac.id.dtsdesigntolayout;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ResetPassword extends AppCompatActivity {
+
+    EditText editTextCode, editTextNewPass, editTextConfirmNewPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset_password);
+
+        editTextCode = findViewById(R.id.edt_txt_code);
+        editTextNewPass = findViewById(R.id.edt_new_password);
+        editTextConfirmNewPass = findViewById(R.id.edt_confirm_password);
+
     }
 
     public void postChangePassword(View view) {
-        Intent i = new Intent(ResetPassword.this, SuccessActivity.class);
-        startActivity(i);
+        // Validasi kosong
+        if(TextUtils.isEmpty(editTextCode.getText().toString().trim()) ||
+            TextUtils.isEmpty(editTextNewPass.getText().toString()) ||
+            TextUtils.isEmpty(editTextConfirmNewPass.getText().toString())) {
+            Toast.makeText(view.getContext(), "Tidak boleh ada isian yang kosong",
+                    Toast.LENGTH_LONG).show();
+        }
+        // Cek inputan new dan confirm
+        else if (editTextNewPass.getText().toString() != editTextConfirmNewPass.getText().toString()) {
+            Toast.makeText(view.getContext(), "Password tidak sama!", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Intent i = new Intent(ResetPassword.this, SuccessActivity.class);
+            startActivity(i);
+        }
     }
 }
