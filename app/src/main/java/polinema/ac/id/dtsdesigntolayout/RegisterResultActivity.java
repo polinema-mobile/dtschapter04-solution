55bdd924a447439b31ab9c1d80563a4dcb85bbce
od3ng <lepengdados@gmail.com>
2019-07-02 14:59:53 +0700

    add comment

--- app/src/main/java/polinema/ac/id/dtsdesigntolayout/RegisterResultActivity.java
+++ app/src/main/java/polinema/ac/id/dtsdesigntolayout/RegisterResultActivity.java
@@ -6,17 +6,22 @@ import android.widget.TextView;
 
 public class RegisterResultActivity extends AppCompatActivity {
 
+    //inisialisasi variabel
     TextView tvResultNama, tvResultTanggalLahir, tvResultJenisKelamin, tvResultUsername;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_result);
+        // assign view
         tvResultNama = findViewById(R.id.tvResultNama);
         tvResultTanggalLahir = findViewById(R.id.tvResultTanggalLahir);
         tvResultJenisKelamin = findViewById(R.id.tvResultJenisKelamin);
         tvResultUsername = findViewById(R.id.tvResultUsername);
+
+        //get string array berdasarkan key
         String[] stringArray = getIntent().getStringArrayExtra(RegisterActivity.Key_RegisterActivity);
+        //set value to textview
         tvResultNama.setText(stringArray[0]);
         tvResultTanggalLahir.setText(stringArray[1]);
         tvResultJenisKelamin.setText(stringArray[2]);
